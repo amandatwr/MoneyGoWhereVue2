0787afa22b812e7c62c97886ef00dd9de60d2a5a
tammygrq <80189126+tammygrq@users.noreply.github.com>
2021-03-27 15:13:36 +0800

    added function to return plan giving highest returns

--- src/components/Recommender.vue
+++ src/components/Recommender.vue
@@ -8,7 +8,7 @@
         <br><br>
         <div id="list1" class="dropdown-check-list" tabindex="100">
             <span class="anchor" @click="displayBox()">Select Preferred Financial Institution</span>
-            <ul class="items" style="text-align:left">
+            <ul class="items" style="text-align:left;">
                 <li><input type="checkbox" id="allPlans" @click="selectAll()" /> Select All</li>
                 <li v-for="plan in listOfPlans" v-bind:key="plan">
                     <input type="checkbox" name="provider" v-bind:value="plan" v-model="selectedPlans" /> {{plan.provider}}
@@ -16,34 +16,41 @@
             </ul>
         </div>
         <br><br>
-        <button id="btn" type="button" @click="show_form = true; findPlans()">Next</button>
+        <button id="btn" type="button" @click="show_form = true; findPlans(); calculateReturns()">Next</button>
         <br><br>
         <div v-if="show_form">
-            <h2 id="text">Your Curated Savings Plan:</h2>
+            <h1 id="text">Your Curated Savings Plan:</h1>
             <p id="text">Show plan which gives the highest return</p>
             <br>
-            <div class="w3-row" style="padding:20px 50px 50px 50px">
-                <div class="w3-col s6 w3-center" style="padding-left:100px">
-                    <h5 id="text">New Design of Plan:<br>Plan Name<br>Provider Logo<br>Interest Rate<br>Find Out More</h5>
-                </div>
-                <div class="w3-col s6 w3-center" style="padding-right:100px">
+            <div class="w3-row" style="padding:0px 50px 50px 50px">
+                <ul style="padding:0px">
+                    <li v-for="plan in recommendedPlans.slice(0,1)" v-bind:key="plan">
+                        <div class="w3-col s6 w3-center" style="padding:0px 80px 80px 80px">
+                            <img style="width:100%; height:300px" v-bind:src="plan.logo" alt="logo">
+                            <p id="name"><b>{{plan.name}}</b></p>
+                            <p id="name">Interest Rate: {{(plan.interest_pa*100).toFixed(2)}}%</p>
+                            <button class="learnMore" v-bind:id="plan.id" v-on:click="route($event)">Find Out More</button>
+                        </div>
+                    </li>
+                </ul>
+                <div class="w3-col s6 w3-center" style="display:inline; padding-right:100px; padding-top:200px; justify-content: center">
                     <h3 id="text">Your Projected Returns:</h3>
                     <br><br>
                     <h5 id="text">Display Returns Here</h5>
                 </div>
             </div>
             <br><br>
-            <h2 id="text">You may also like...</h2>
+            <h1 id="text">You may also like...</h1>
             <p id="text">Show next 3 plans with the highest returns</p>
-            <div class="w3-row" style="padding:20px 50px 50px 50px">
+            <div class="w3-row" style="padding:20px 70px 70px 70px">
                 <ul style="padding:0px">
-                    <li v-for="plan in recommendedPlans" v-bind:key="plan">
+                    <li v-for="plan in recommendedPlans.slice(1,4)" v-bind:key="plan">
                         <div class="w3-col s4 w3-center" style="padding:30px">
                             <img style="width:100%; height:180px" v-bind:src="plan.logo" alt="logo">
                             <br><br>
                             <p id="name"><b>{{plan.name}}</b></p>
                             <p id="name">Interest Rate: {{(plan.interest_pa*100).toFixed(2)}}%</p>
-                            <p id="name">Projected Returns:</p>
+                            <p id="name">Projected Returns: ${{plan.returns}}</p>
                             <button class="learnMore" v-bind:id="plan.id" v-on:click="route($event)">Find Out More</button>
                         </div>
                     </li>
@@ -95,6 +102,14 @@ export default {
                     this.recommendedPlans.push(this.selectedPlans[i]);
                 }
             }
+        },
+        calculateReturns: function() {
+            for (let i = 0; i < this.recommendedPlans.length; i++) {
+                var interest = this.recommendedPlans[i].interest_pa;
+                var projectedReturns = (1+interest) * this.amount;
+                this.recommendedPlans[i].returns = projectedReturns;
+            }
+            this.recommendedPlans.sort(function(a, b) { return b.returns - a.returns });
         },
         selectAll: function() {
             var selected = document.getElementById("allPlans")
@@ -245,5 +260,12 @@ button {
     font-size: 30px;
     text-align: center;
     font-family: Optima;
+}
+
+img {
+    padding-top: 20px;
+    padding-bottom: 20px;
+    padding-left: 40px;
+    padding-right: 40px;
 }
 </style>
