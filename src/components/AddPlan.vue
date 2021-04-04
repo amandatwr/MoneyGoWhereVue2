5b5ce4653faa37267c070f58a49d7b00395ebca6
dakshitaa <e0420143@u.nus.edu>
2021-04-04 13:00:46 +0800

    interim edit function, UI up, functionality in progress

--- src/components/AddPlan.vue
+++ src/components/AddPlan.vue
@@ -1,44 +1,61 @@
 <template>
-	<div class="addNew">
-		<button class="addButton" @click="show_form = true"></button>
-		<div v-if="show_form" class="centerimg2">
-			<!-- when the form is submitted, add the plan to the database -->
-			<!-- .prevent prevents the submission event from "reloading" the page -->
-			<v-card class="card"> 
-                <br>
-            <form @submit.prevent="addPlan">
-				<label>Add a Plan: </label>
-				<!-- Bidirectionally bind planSelect to this select element -->
-				<select v-model="planSelect" class="drawbox">
-					<option v-for="plan in plans" v-bind:key="plan.name" v-bind:value="plan.name">{{plan.name}}</option>
-					<!-- <option value="other">Other</option> -->
-				</select>
-                
-				<!-- <div v-if="planSelect != 'other'"> -->
-					<!-- You are adding {{planSelect}} -->
-				<!-- </div> -->
-				<!-- <div v-else>
+  <div class="addNew">
+    <button class="addButton" @click="show_form = true"></button>
+    <div v-if="show_form" class="centerimg2">
+      <!-- when the form is submitted, add the plan to the database -->
+      <!-- .prevent prevents the submission event from "reloading" the page -->
+      <v-card class="card">
+        <br />
+        <form @submit.prevent="addPlan">
+          <label>Add a Plan: </label>
+          <!-- Bidirectionally bind planSelect to this select element -->
+          <select v-model="planSelect" class="drawbox">
+            <option
+              v-for="plan in plans"
+              v-bind:key="plan.planID"
+              v-bind:value="plan.name"
+            >
+              {{ plan.name }}
+            </option>
+            <!-- <option value="other">Other</option> -->
+          </select>
+
+          <!-- <div v-if="planSelect != 'other'"> -->
+          <!-- You are adding {{planSelect}} -->
+          <!-- </div> -->
+          <!-- <div v-else>
 					The user wants to add a new plan -->
-					<!-- Bidirectionally bind planName, planInterest, planMinYrs to these input elements -->
-					<!-- <input type="text" v-model="planName" placeholder="Enter Plan Name">
+          <!-- Bidirectionally bind planName, planInterest, planMinYrs to these input elements -->
+          <!-- <input type="text" v-model="planName" placeholder="Enter Plan Name">
                     <br>
                     <input type="text" v-model="planProvider" placeholder="Enter Plan Provider">
                     <br>
 					<input type="text" v-model="planInterest" placeholder="Enter Interest Rate">
                     <br>
 					<input type="text" v-model="planMinYrs" placeholder="Enter Minimum No. of Years">
-				</div> --> 
+				</div> -->
 
-				<!-- Bidirectionally bind planAmount -->
-				<br>
-                Amount: <input type="text" v-model="planAmount" placeholder="Enter Amount Saved">
-				<br><br>
-                <button class= "submitbutton" v-on:click="submit">Add Plan</button>
-			</form>
-            </v-card>
-		</div>
-        <br><br><br>
-	</div>
+          <!-- Bidirectionally bind planAmount -->
+          <br />
+          Amount:
+          <input
+            type="text"
+            v-model="planAmount"
+            placeholder="Enter Amount Saved"
+          />
+          <br /><br />
+          <button
+            class="submitbutton"
+            v-on:click="submit"
+            @click="show_form = false"
+          >
+            Add Plan
+          </button>
+        </form>
+      </v-card>
+    </div>
+    <br /><br /><br />
+  </div>
 </template>
 
 <script>
@@ -46,142 +63,143 @@ import db from "../firebase.js";
 import firebase from "firebase";
 
 export default {
-    data() {
-        return {
-            show_form: false,
-            plans: [],
-			planSelect: '',
-			planName: '',
-			planInterest: '',
-			planMinYrs: '',
-			planAmount: '',
-            planProvider:'',
-            planID:'',
-        };
-    },
-    methods: {
-        camelize: function(str) {
-  return str.replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, function(match, index) {
-    if (+match === 0) return ""; // or if (/\s+/.test(match)) for white spaces
-    return index === 0 ? match.toLowerCase() : match.toUpperCase();
-  });
-},
-
-        fetchItems: function() {
-            db.collection('Listings').get().then((querySnapShot) => {
-                let list = {}
-                querySnapShot.forEach(doc => {
-                    list = doc.data()
-                    list.id = doc.id
-                    this.plans.push(list)
-                })
-            })
-        },
-		// Event handler for form submission
-		addPlan: function () {
-			var user = firebase.auth().currentUser;
-            
-
-			// if (this.planSelect == 'other') {
-            //this.planID = this.camelize(this.planName) + "_" + this.planProvider;
-            console.log(this.planSelect);
-            //console.log(this.planAmount);
-			// 	// Create a new plan before adding the plan to the user
-			// 	db.collection('Listings').doc(this.planID).add({
-			// 		name: this.planName, // planID
-            //         provider: this.planProvider,
-			// 		interest_pa: this.planInterest,
-			// 		min_years: this.planMinYrs,
-
-			// 	});
-                
-
-			// 	// Add the plan to the user, with the above created plan
-				// db.collection('TestUsers').doc(user.uid).get().then(doc => {
-                //     let newPlans = doc.data();
-                //     newPlans.push({ planID: this.planID, amount: this.planAmount });
-                //     doc(user.uid).update({
-                //                 plans: newPlans,
-                //         });
-                //     });
-
-			// } else {
-				// Add the plan to the user, with the existing selected plan
-				db.collection('TestUsers').doc(user.uid).update({
-                    plans: firebase.firestore.FieldValue.arrayUnion({ name: this.planSelect, amount: this.planAmount }),
-                });
-                // db.collection('TestUsers').doc(user.uid).add({
-				// 	planID: this.planSelect,
-				// 	amount: this.planAmount,
-				// });
-			//}
-		},
+  data() {
+    return {
+      show_form: false,
+      plans: [],
+      planSelect: "",
+      planName: "",
+      planInterest: "",
+      planMinYrs: "",
+      planAmount: "",
+      planProvider: "",
+      planID: "",
+    };
+  },
+  methods: {
+    camelize: function (str) {
+      return str.replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, function (match, index) {
+        if (+match === 0) return ""; // or if (/\s+/.test(match)) for white spaces
+        return index === 0 ? match.toLowerCase() : match.toUpperCase();
+      });
     },
 
-    created() {
-        this.fetchItems();
-    }
+    fetchItems: function () {
+      db.collection("Listings")
+        .get()
+        .then((querySnapShot) => {
+          let list = {};
+          querySnapShot.forEach((doc) => {
+            list = doc.data();
+            list.id = doc.id;
+            this.plans.push(list);
+          });
+        });
+    },
+    // Event handler for form submission
+    addPlan: function () {
+      var user = firebase.auth().currentUser;
+
+      // if (this.planSelect == 'other') {
+      //this.planID = this.camelize(this.planName) + "_" + this.planProvider;
+      console.log(this.planSelect);
+      //console.log(this.planAmount);
+      // 	// Create a new plan before adding the plan to the user
+      // 	db.collection('Listings').doc(this.planID).add({
+      // 		name: this.planName, // planID
+      //         provider: this.planProvider,
+      // 		interest_pa: this.planInterest,
+      // 		min_years: this.planMinYrs,
+
+      // 	});
+
+      // 	// Add the plan to the user, with the above created plan
+      // db.collection('TestUsers').doc(user.uid).get().then(doc => {
+      //     let newPlans = doc.data();
+      //     newPlans.push({ planID: this.planID, amount: this.planAmount });
+      //     doc(user.uid).update({
+      //                 plans: newPlans,
+      //         });
+      //     });
+
+      // } else {
+      // Add the plan to the user, with the existing selected plan
+      db.collection("TestUsers")
+        .doc(user.uid)
+        .update({
+          plans: firebase.firestore.FieldValue.arrayUnion({
+            name: this.planSelect,
+            amount: this.planAmount,
+          }),
+        });
+      // db.collection('TestUsers').doc(user.uid).add({
+      // 	planID: this.planSelect,
+      // 	amount: this.planAmount,
+      // });
+      //}
+    },
+  },
+
+  created() {
+    this.fetchItems();
+  },
 };
 </script>
 
 <style scoped>
-
 .banner-buttons {
-	width: 100%;
-	padding: 0px 180px;
-	display: flex;
-	flex-direction: row;
-	justify-content: space-between;
-	text-align: center;
+  width: 100%;
+  padding: 0px 180px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  text-align: center;
 }
 
 .addButton {
-	background-image: url("../assets/addnew.png");
-	background-size: 64px 64px;
-	height: 64px;
-	width: 64px;
-    color:black;
-    margin-left: auto;
-	margin-right: auto;
-    margin-bottom: 50px;
+  background-image: url("../assets/addnew.png");
+  background-size: 64px 64px;
+  height: 64px;
+  width: 64px;
+  color: black;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 50px;
 }
 
 .addNew {
-	text-align: center;
-	margin-left: auto;
-	margin-right: auto;
-	width: 70%;
-    font-size: 18px;
-    justify-content:center;
-    font-family: Optima;
+  text-align: center;
+  margin-left: auto;
+  margin-right: auto;
+  width: 70%;
+  font-size: 18px;
+  justify-content: center;
+  font-family: Optima;
 }
 
 .centerimg2 {
-	display: block;
-	margin-left: auto;
-	margin-right: auto;
-	width: 60%;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 60%;
 }
 
 .card {
   margin: 0px 20px;
   padding: 5%;
   height: 200px;
-
 }
 
 .drawbox {
-    border-bottom: 2px solid grey;
-    width: 150px;
-
+  border-bottom: 2px solid grey;
+  width: 150px;
 }
 
 .submitbutton {
-    border: 1px solid grey;
-    background-color: white;
-    color: #545454;
-    align-items: center;
-    display: inline-block;
+  border: 1px solid grey;
+  background-color: white;
+  color: #545454;
+  align-items: center;
+  display: inline-block;
 }
-
 </style>
--- src/components/MyPlansRect.vue
+++ src/components/MyPlansRect.vue
@@ -1,29 +1,43 @@
 <template>
-<div id="listings">
-  <div class="w3-row" style="padding:20px 50px 50px 50px">
-  
-    
-      <ul style="padding:0px">
-         
-          <li v-for="plan in plans" v-bind:key="plan">
-            
-            <div class="tooltip">
-              <v-card class="myplan-card">
-                  <p id="name"><b>{{plan.name}}</b></p>
-                  <p id="provider">{{plan.provider}}</p>
-                  <h3 text-align:center id = "amount"> {{plan.amount}}</h3>
-                  <span class="tooltiptext"> Interest Rate: {{plan.interest}}<br>Capital Guaranteed: {{plan.capital_guaranteed}}<br> Min. no. of Years: {{plan.min_years}}</span>
-              </v-card>
-            </div>
-           
-          </li>
-     
+  <div id="listings">
+    <div class="w3-row" style="padding: 20px 50px 50px 50px">
+      <ul style="padding: 0px">
+        <li v-for="plan in plans" v-bind:key="plan">
+          <div class="tooltip">
+            <v-card class="myplan-card">
+              <button class="editButton" @click="show_form = true"></button>
+              <p id="name">
+                <b>{{ plan.name }}</b>
+              </p>
+              <p id="provider">{{ plan.provider }}</p>
+              <h3 text-align:center id="amount">{{ plan.amount }}</h3>
+              <span class="tooltiptext">
+                Interest Rate: {{ plan.interest }}<br />Capital Guaranteed:
+                {{ plan.capital_guaranteed }}<br />
+                Min. no. of Years: {{ plan.min_years }}</span
+              >
+            </v-card>
+          </div>
+
+          <div v-if="show_form">
+            <!-- when the form is submitted, edit plan amount to the database -->
+            <!-- .prevent prevents the submission event from "reloading" the page -->
+            <v-card class="editcard">
+              <form @submit.prevent="MySavings">
+                <label> Edit Amount: </label>
+                <input
+                  type="text"
+                  v-model="planAmount"
+                  placeholder="Enter Value"
+                />
+              </form>
+            </v-card>
+          </div>
+        </li>
       </ul>
-      
-  
-  </div>
+    </div>
     <!--<input v-model="textValue" placeholder="input amount">-->
-</div>
+  </div>
 </template>
 
 
@@ -32,71 +46,83 @@ import database from "../firebase.js";
 import firebase from "firebase";
 
 export default {
-    name: 'MyPlan',
+  name: "MyPlan",
   props: {
-      item: Object
+    item: Object,
   },
   data() {
     return {
-      textValue:"",
-      plans: [],      
-    }
-  }, 
+      textValue: "",
+      plans: [],
+      show_form: false,
+    };
+  },
   methods: {
     fetchItems: function () {
       // Log user account creation date
       var user = firebase.auth().currentUser;
       var signupDate = new Date(user.metadata.creationTime);
       var currDate = new Date();
-      this.days = this.getDateDiff(currDate, signupDate);      
+      this.days = this.getDateDiff(currDate, signupDate);
 
-       database
+      database
         .collection("TestUsers")
         .doc(user.uid)
         .get()
         .then((querySnapShot) => {
-          var plans = querySnapShot.data().plans
-          for ( let i = 0 ; i < plans.length ; i++ ) {
-            var planID = plans[i].planID
+          var plans = querySnapShot.data().plans;
+          for (let i = 0; i < plans.length; i++) {
+            var planID = plans[i].planID;
             database
               .collection("Listings")
               .doc(planID)
               .get()
               .then((listing) => {
                 var listingDetails = listing.data();
-                var planDetails = {}
+                var planDetails = {};
                 planDetails["name"] = listingDetails.name;
                 planDetails["provider"] = listingDetails.provider;
-                planDetails["interest"] = this.round((100 * (listingDetails.interest_pa)), 2);
-                planDetails["capital_guaranteed"] = listingDetails.capital_guaranteed;
+                planDetails["interest"] = this.round(
+                  100 * listingDetails.interest_pa,
+                  2
+                );
+                planDetails["capital_guaranteed"] =
+                  listingDetails.capital_guaranteed;
                 planDetails["min_years"] = listingDetails.min_years;
-                planDetails["amount"] = this.formatter().format(plans[i].amount);
-                planDetails['dateSaved'] = plans[i].dateSaved.toDate().toLocaleDateString();
-                planDetails['dateWithdraw'] = this.getReturnsDate(plans[i].dateSaved.toDate(), listingDetails.min_years).toLocaleDateString();
+                planDetails["amount"] = this.formatter().format(
+                  plans[i].amount
+                );
+                planDetails["dateSaved"] = plans[i].dateSaved
+                  .toDate()
+                  .toLocaleDateString();
+                planDetails["dateWithdraw"] = this.getReturnsDate(
+                  plans[i].dateSaved.toDate(),
+                  listingDetails.min_years
+                ).toLocaleDateString();
                 this.plans.push(planDetails);
-          
-        })}
-    
-    });
-
+              });
+          }
+        });
     },
-    
+    setItems: function () {},
 
     round: function (value, decimals) {
-        return Number(Math.round(value +'e'+ decimals) +'e-'+ decimals).toFixed(decimals);
+      return Number(
+        Math.round(value + "e" + decimals) + "e-" + decimals
+      ).toFixed(decimals);
     },
-     getReturnsDate: function (dateSaved, numYears) {
-                return (new Date(
-                  dateSaved.setFullYear(new Date().getFullYear() + numYears)
-                ))
-     },
+    getReturnsDate: function (dateSaved, numYears) {
+      return new Date(
+        dateSaved.setFullYear(new Date().getFullYear() + numYears)
+      );
+    },
 
-     getDateDiff: function (from, to) {
+    getDateDiff: function (from, to) {
       // console.log(from, to);
       return Math.floor((from - to) / 86400000);
     },
 
-  formatter: function () {
+    formatter: function () {
       return new Intl.NumberFormat("en-US", {
         style: "currency",
         currency: "USD",
@@ -104,14 +130,26 @@ export default {
       });
     },
   },
-    created() {
+  created() {
     this.fetchItems();
-  }
-       
-}
+  },
+};
 </script>
 
 <style scoped>
+.editButton {
+  background-image: url("../assets/editplan.png");
+  background-size: 16px 16px;
+  background-repeat: no-repeat;
+  height: 16px;
+  width: 16px;
+  color: black;
+  margin-left: 270px;
+  /* margin-bottom: 270px; */
+  margin-top: 10px;
+  margin-right: 10px;
+}
+
 .tooltip {
   position: relative;
   display: inline-block;
@@ -155,37 +193,42 @@ h6 {
   color: black;
   text-align: center;
   text-justify: auto;
-  
 }
 
 #listings {
-    padding-top: 100px;
-    text-align:center;
+  padding-top: 100px;
+  text-align: center;
 }
 
 #name {
-    font-size: 18px;
-    white-space: nowrap;
-    text-align:center;
+  font-size: 18px;
+  white-space: nowrap;
+  text-align: center;
 }
 
 #provider {
-    font-size: 14px;
-    white-space: nowrap;
-    text-align:center;
+  font-size: 14px;
+  white-space: nowrap;
+  text-align: center;
 }
 
-ul{
+ul {
   display: flex;
   flex-wrap: wrap;
   justify-content: space-around;
-
 }
 
 li {
   flex: 1 0 25%;
   margin: 15px;
-} 
+}
+
+p {
+  margin-top: 0px;
+  margin-bottom: 10px;
+  margin-left: auto;
+  margin-right: auto;
+}
 
 /* #flex-container {
   display:flex;
@@ -205,9 +248,9 @@ li {
 } */
 
 .myplan-card {
-  margin: 0 25px;
+  /* margin: 0 25px; */
   height: 200px;
   width: 130% !important;
-  padding-top: 35px;
+  /* padding-top: 35px; */
 }
 </style>
